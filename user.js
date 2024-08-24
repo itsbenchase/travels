522103218d1240cd930f173b7403ae98f7d3e40c
itsbenchase <ben.chase@benchase.info>
2024-08-24 16:17:26 -0400

    now this is working a bit

--- user.js
+++ user.js
@@ -1,120 +1,184 @@
 var user = "null";
+var userDist = 0.00;
 
-function loadUser()
+const users = [];
+const userAgencies = [];
+const userRoutes = [];
+const userRouteStart = [];
+const userRouteEnd = [];
+const userFile = [];
+
+const agencies = [];
+const fullAgencies = [];
+
+const ediFile = [];
+const stopIds = [];
+const stopNames = [];
+const stopLats = [];
+const stopLons = [];
+const stopLines = [];
+const stopOrders = [];
+
+// cumulative
+const cumLats = [];
+const cumLons = [];
+
+function getUsers()
 {
-  const userFileUrl = ("https://travel.eliotindex.org/users/" + user + ".txt"); // provide file location
-      fetch(userFileUrl)
-          .then(r => r.text())
-          .then((text) => {
-              const userFile = text.split("\n");
-              delete userFile[userFile.length - 1]; // last one is blank
+  const userListUrl = ("https://travel.eliotindex.org/users.txt"); // provide file location
+  fetch(userListUrl)
+    .then(r => r.text())
+    .then((text) => {
+      const userList = text.split("\n");
+      //userList.pop();
 
-              intoArray(userFile);
-          })
-}
-
-function findIndex()
-{
-    // clear arrays
-    stopIds.length = 0;
-    stopNames.length = 0;
-    stopLats.length = 0;
-    stopLons.length = 0;
-    stopLines.length = 0;
-    stopOrders.length = 0;
-    segStrings.length = 0;
-    cumLats.length = 0;
-    cumLons.length = 0;
-
-    selectedRoute = document.querySelector('#routeDrop');
-    code = selectedRoute.value;
+      for (let i = 0; i < userList.length; i++)
+      {
+        var data = userList[i];
+        users.push(data);
+        document.getElementById("userDrop").innerHTML += ("<option value=" + users[i] + ">" + users[i] + "</option>");
+      }
+    })
 
     // gets full agency name
     const agencyFileUrl = ("https://dev.eliotindex.org/agencies.txt"); // provide file location
     fetch(agencyFileUrl)
-        .then(r => r.text())
-        .then((text) => {
-            const agencyFile = text.split("\n");
-            agencyFile.pop();
+      .then(r => r.text())
+      .then((text) => {
+        const agencyFile = text.split("\n");
+        agencyFile.pop();
 
-            const agencies = [];
-            const fullAgencies = [];
-
-            for (let i = 0; i < agencyFile.length; i++)
-            {
-                var data = agencyFile[i];
-                agencies.push(data.substring(0, data.indexOf(";")));
-                data = data.substr(data.indexOf(";") + 1);
-                fullAgencies.push(data);
-            }
-            
-            for (let i = 0; i < agencies.length; i++)
-            {
-                if (agencies[i] == agency)
-                {
-                    document.getElementById("agency").innerHTML = ("<b>Agency: </b>" + fullAgencies[i] + " (" + agencies[i] + ")");
-                }
-            }
-        })
-
-    const ediFileUrl = ("https://dev.eliotindex.org/files/" + agency +  "-edi.txt"); // provide file location
-    fetch(ediFileUrl)
-        .then(r => r.text())
-        .then((text) => {
-            const ediFile = text.split("\n");
-            delete ediFile[ediFile.length - 1]; // last one is blank
-
-            intoArray(ediFile);
-        })
+        for (let i = 0; i < agencyFile.length; i++)
+        {
+          var data = agencyFile[i];
+          agencies.push(data.substring(0, data.indexOf(";")));
+          data = data.substr(data.indexOf(";") + 1);
+          fullAgencies.push(data);
+        }
+      })
 }
 
-function intoArray(ediFile)
+function loadUser()
 {
-    ediFile.pop(); // we might have a bug
-    for (let i = 0; i < ediFile.length; i++)
-    {
-        var data = ediFile[i];
-        stopIds.push(data.substring(0, data.indexOf(";")));
-        data = data.substr(data.indexOf(";") + 1);
-        stopNames.push(data.substring(0, data.indexOf(";")));
-        data = data.substr(data.indexOf(";") + 1);
-        stopLats.push(data.substring(0, data.indexOf(";")));
-        data = data.substr(data.indexOf(";") + 1);
-        stopLons.push(data.substring(0, data.indexOf(";")));
-        data = data.substr(data.indexOf(";") + 1);
-        stopLines.push(data.substring(0, data.indexOf(";")));
-        data = data.substr(data.indexOf(";") + 1);
-        stopOrders.push(data);
-    }
+  selectedUser = document.querySelector('#userDrop');
+  user = selectedUser.value;
+  
+  const userFileUrl = ("https://travel.eliotindex.org/users/" + user + ".txt"); // provide file location
+    fetch(userFileUrl)
+      .then(r => r.text())
+      .then((text) => {
+        const userFile = text.split("\n");
+        //delete userFile[userFile.length - 1]; // last one is blank
 
-    getStops(ediFile)
+        intoAgencies(userFile);
+      })
 }
 
-function getStops(ediFile)
+function intoAgencies(userFile)
 {
-  // list stops on route
-  var cumCount = 1;
-
-  for (let i = 0; i < ediFile.length; i++)
+  //userFile.pop(); // we might have a bug
+  for (let i = 0; i < userFile.length; i++)
   {
-    if (stopLines[i] == code)
+    var data = userFile[i];
+    userAgencies.push(data.substring(0, data.indexOf(",")));
+    data = data.substr(data.indexOf(",") + 1);
+    userRoutes.push(data.substring(0, data.indexOf(",")));
+    data = data.substr(data.indexOf(",") + 1);
+    userRouteStart.push(data.substring(0, data.indexOf(",")));
+    data = data.substr(data.indexOf(",") + 1);
+    userRouteEnd.push(data);
+  }
+
+  var agencyAdded = false;
+
+  for (let i = 0; i < agencies.length; i++)
+  {
+    for (let j = 0; j < userAgencies.length; j++)
     {
-      cumLats.push(stopLats[i]);
-      cumLons.push(stopLons[i]);
-      cumulative(i, cumCount);
-      cumCount++;
+      if (agencies[i] == userAgencies[j])
+      {
+        if (!agencyAdded)
+        {
+          document.getElementById("info").innerHTML += ("<h3>" + fullAgencies[i] + " (" + agencies[i] + ")</h3>");
+          agencyAdded = true;
+        }
+
+        getAgencyFile(userAgencies[j]);
+      }
     }
   }
 }
 
-function cumulative(pos, cumCount)
+function getAgencyFile(agency)
+{
+  const ediFileUrl = ("https://dev.eliotindex.org/files/" + agency + "-edi.txt"); // provide file location
+  fetch(ediFileUrl)
+    .then(r => r.text())
+    .then((text) => {
+      const ediFile = text.split("\n");
+      //delete ediFile[ediFile.length - 1]; // last one is blank
+      
+      for (let k = 0; k < ediFile.length; k++)
+      {
+        var data = ediFile[k];
+        stopIds.push(data.substring(0, data.indexOf(";")));
+        data = data.substring(data.indexOf(";") + 1);
+        stopNames.push(data.substring(0, data.indexOf(";")));
+        data = data.substring(data.indexOf(";") + 1);
+        stopLats.push(data.substring(0, data.indexOf(";")));
+        data = data.substring(data.indexOf(";") + 1);
+        stopLons.push(data.substring(0, data.indexOf(";")));
+        data = data.substring(data.indexOf(";") + 1);
+        stopLines.push(data.substring(0, data.indexOf(";")));
+        data = data.substring(data.indexOf(";") + 1);
+        stopOrders.push(data);
+      }
+
+      listRoutes();
+    })
+}
+
+function listRoutes()
+{
+  for (let j = 0; j < userRoutes.length; j++)
+  {
+    // list stops on route
+    for (let i = 0; i < stopLines.length; i++)
+    {
+      if (stopLines[i] == userRoutes[j])
+      {
+        cumLats.push(stopLats[i]);
+        cumLons.push(stopLons[i]);
+      }
+    }
+    cumulative(userRoutes[j], userRouteStart[j], userRouteEnd[j]);
+  }
+}
+
+function cumulative(code, segStartOrder, segEndOrder)
 {
   // haversine formula loop
   // spherical trig cause this is the globe
   // cumLats.length will increase by 1 each run
+
   var dist = 0;
+  var cumCount = 1;
+
+  var segStartDist = 0.00;
+  var segEndDist = 0.00;
+
   for (let i = 1; i < cumLats.length; i++)
   {
+    // display
+    if (cumCount == segStartOrder)
+    {
+      segStartDist = dist;
+    }
+    if (cumCount == segEndOrder)
+    {
+      segEndDist = dist;
+    }
+
     var lon1 = toRadians(Math.abs(cumLons[i - 1]));
     var lon2 = toRadians(Math.abs(cumLons[i]));
     var lat1 = toRadians(Math.abs(cumLats[i - 1]));
@@ -126,101 +190,18 @@ function cumulative(pos, cumCount)
     var r = 3963;
 
     dist += c * r;
-  }
 
-  // full line haversine
-  var firstLon = toRadians(cumLons[0]);
-  var lastLon = toRadians(cumLons[cumLons.length - 1]);
-  var firstLat = toRadians(cumLats[0]);
-  var lastLat = toRadians(cumLats[cumLats.length - 1]);
-  var difflon = 0; // placeholder
-  var difflat = 0; // placeholder
-
-  // longitude math
-  if (firstLon >= 0 && lastLon >= 0)
-  {
-    difflon = lastLon - firstLon;
-  }
-  else if (firstLon <= 0 && lastLon <= 0)
-  {
-    lastLon = Math.abs(lastLon);
-    firstLon = Math.abs(firstLon);
-    difflon = lastLon - firstLon;
-  }
-  else if (firstLon <= 0 && lastLon >= 0)
-  {
-    difflon = Math.abs(firstLon - lastLon);
-  }
-  else if (firstLon >= 0 && lastLon <= 0)
-  {
-    difflon = Math.abs(lastLon - firstLon);
-  }
-
-  // latitude math
-  if (firstLat >= 0 && lastLat >= 0)
-  {
-    difflat = lastLat - firstLat;
-  }
-  else if (firstLat <= 0 && lastLat <= 0)
-  {
-    lastLat = Math.abs(lastLat);
-    firstLat = Math.abs(firstLat);
-    difflat = lastLat - firstLat;
-  }
-  else if (firstLat <= 0 && lastLat >= 0)
-  {
-    difflat = Math.abs(firstLat - lastLat);
-  }
-  else if (firstLat >= 0 && lastLat <= 0)
-  {
-    difflat = Math.abs(lastLat - firstLat);
-  }
-
-  var a1 = Math.pow(Math.sin(difflat / 2), 2) + Math.cos(firstLat) * Math.cos(lastLat) * Math.pow(Math.sin(difflon / 2), 2);      
-  var c1 = 2 * Math.asin(Math.sqrt(a1));
-  var r1 = 3963;
-  var lineDist = c1 * r1;
-
-  // calculate the edi
-  var edi = dist / lineDist;
-  edi = Math.round(edi * 100.0) / 100.0;
-
-  if (edi < 1 || cumCount == 1) // fixes
-  {
-      edi = 1.0;
+    cumCount++;
   }
 
   // dist rounded for display
+  dist = segEndDist - segStartDist;
   dist = Math.round(dist * 100.0) / 100.0;
 
-  // display
-  if (isSegment)
-  {
-    if (cumCount == segStartOrder)
-    {
-      segStartDist = dist;
-    }
-    if (cumCount == segEndOrder)
-    {
-      segEndDist = dist;
-    }
-    if (cumCount >= segStartOrder && cumCount <= segEndOrder)
-    {
-      document.getElementById("stops").innerHTML += ("<br><span style=\"background-color: #88ff7e\">" + cumCount + " | " + stopNames[pos] + " (" + stopIds[pos] + ") (" + dist + " mi.)</span>");
-    }
-    else
-    {
-      document.getElementById("stops").innerHTML += ("<br>" + cumCount + " | " + stopNames[pos] + " (" + stopIds[pos] + ") (" + dist + " mi.)");
-    }
-    document.getElementById("length").innerHTML = ("<b>Length: </b>" + (segEndDist - segStartDist) + " miles / ");
-  }
-  else
-  {
-    document.getElementById("length").innerHTML = ("<b>Length: </b>");
-    document.getElementById("stops").innerHTML += ("<br>" + cumCount + " | " + stopNames[pos] + " (" + stopIds[pos] + ") (" + dist + " mi.)");
-  }
+  userDist += dist;
 
-  dist = segEndDist - segStartDist;
+  document.getElementById("info").innerHTML += (code + " - " + dist + " mi.<br>");
+  document.getElementById("stats").innerHTML += ("User <b>" + user + "</b> has logged <b>" + userDist + "</b> miles in total.");
 }
 
 function toRadians(degrees)
